5e6a15c93660e18d8661d3f3fcfd50f81a5e0744
Mooorphy <kubes1.kp@gmail.com>
2025-09-19 22:40:18 +0200

    update

--- src/app/components/projectGrid.js
+++ src/app/components/projectGrid.js
@@ -7,9 +7,8 @@ export default function projectGrid() {
         <ProjectCard link="https://mw-service.pl/" topic="mw-service.pl" content="Projekt witryny internetowej dla polskiego przedsiębiorstwa MW Service."/>
         <ProjectCard link="https://storbeed.itch.io/gucio-simulation" topic="Gucio: Simulation" content="Drugi projekt grupy storbeed, liniowa gra o tematyce horrorowej."/>
     
-        <ProjectCard link="https://github.com/Moooorphy/BarrelsInvestment" topic="BarrelInvestment" content="Strona służąca do śledzenia postępu w inwestycji, wykorzystująca technikę scrapowania stron internetowych."/>
-        <ProjectCard link="https://youtu.be/2ipXjsPVHmI?si=_9kRaanwxtt0Bclg&t=1110" topic="Birthday Project" content="Fanowski projekt gry VR stworzony przez grupę storbeed."/>
-        
+        <ProjectCard link="https://youtu.be/2ipXjsPVHmI?si=_9kRaanwxtt0Bclg&t=1110" topic="Birthday Project" content="Fanowski projekt gry VR stworzony przez grupę storbeed."/> 
+        <ProjectCard link="" topic="..." content="In coming..." />
         </div>
     
     );
